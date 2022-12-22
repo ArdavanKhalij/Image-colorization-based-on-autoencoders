8fb73f678e81422840605ca66afc4aacc60d0a47
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-12-22 14:53:06 +0100

    Update cnn.py

--- Ardavan/cnn.py
+++ Ardavan/cnn.py
@@ -1,3 +1,5 @@
+######################################################################
+# Libraries
 import kornia as kornia
 import numpy as np
 import torch
@@ -27,26 +29,51 @@ from torchvision import datasets, transforms
 import os, shutil, time
 from torch.utils.data import DataLoader
 
+######################################################################
 # Device Configuration
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-train_path = 'C:/Users/xhuli/OneDrive/Desktop/Education/VUB/3rd Semester/Deep Learning/Project/images/'
-
+train_path = 'Images/train/'
 train_transform = transforms.Compose([
     transforms.Resize((256, 256)),  # can experiment
     transforms.ToTensor(),  # divide the pixel by 255
 ])
+
+######################################################################
 # Loading the data
 train_dataset = torchvision.datasets.ImageFolder(root=train_path, transform=train_transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=72, shuffle=True)
 
+######################################################################
 # Convert RDG to LAB space
 # assign the L channel to the x vector
 x = []  # array of tensors of  L
 y = []  # array of tensors of A and B
 for idx, (data, target) in enumerate(train_loader):
     for i in data:
-        lab = kornia.color.rgb_to_lab(i/255)
+        lab = kornia.color.rgb_to_lab(i / 255)
         x.append(lab[0])
         y.append(lab[1:3])
+
+newInput = []
+for i in x:
+    newX = i
+    newX = newX[None, :]
+    newInput.append(newX)
+    
+######################################################################
+# Creating the model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer = torch.nn.UpsamplingNearest2d(size=None, scale_factor=2)
+
+    def forward(self, x):
+        x = self.layer(x)
+        return x
+
+
+######################################################################
+# Initialize the network
+model = CNN().to(device)
+print("Model Shape= ", model(newInput[0]).shape)
+print("Y shape=  ", y[0].shape)
